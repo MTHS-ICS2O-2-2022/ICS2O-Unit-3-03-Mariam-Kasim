51ddc061e3df1a613ed7ef2073b82e8dcec8777f
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-04-12 16:21:28 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Apr 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the volume of a sphere
+ */
+function calculate() {
+  // input
+  const radius = parseFloat(document.getElementById("radius").value)
+
+  // process
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+
+  // output
+  document.getElementById('volume').innerHTML = 'volume is: ' + volume.toFixed(2) + ' mm³'
+}
